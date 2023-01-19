0da20bee7c7c328a38602b87dd0ad3139bd5e549
YukaNagata <yuka.nagata@woven-planet.global>
2023-01-19 11:58:14 +0900

    lava game nagata

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ball::Start()
+extern void ball_Start_m1CD24200BCAB6E776C7B49D8A61BD558396C2D09 (void);
+// 0x00000002 System.Void ball::Update()
+extern void ball_Update_m63087F3133C02AD7502C1F52B6BC8A38E3C95516 (void);
+// 0x00000003 System.Void ball::.ctor()
+extern void ball__ctor_m596394A37522CF45E95FC7561A8D3BED786A7E09 (void);
+// 0x00000004 System.Void BallPosition::Start()
+extern void BallPosition_Start_m905E56A2BA9CE2667DE4AD98E6233180EEE56167 (void);
+// 0x00000005 System.Void BallPosition::Update()
+extern void BallPosition_Update_m69430AE1E5E0D988AA7EEB4B222013332BF3F97F (void);
+// 0x00000006 System.Void BallPosition::.ctor()
+extern void BallPosition__ctor_m0932AFE369E93492A715C48C4974D38B7CB827FC (void);
+// 0x00000007 System.Void HelloWorld::Start()
+extern void HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247 (void);
+// 0x00000008 System.Void HelloWorld::Update()
+extern void HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5 (void);
+// 0x00000009 System.Void HelloWorld::.ctor()
+extern void HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	ball_Start_m1CD24200BCAB6E776C7B49D8A61BD558396C2D09,
+	ball_Update_m63087F3133C02AD7502C1F52B6BC8A38E3C95516,
+	ball__ctor_m596394A37522CF45E95FC7561A8D3BED786A7E09,
+	BallPosition_Start_m905E56A2BA9CE2667DE4AD98E6233180EEE56167,
+	BallPosition_Update_m69430AE1E5E0D988AA7EEB4B222013332BF3F97F,
+	BallPosition__ctor_m0932AFE369E93492A715C48C4974D38B7CB827FC,
+	HelloWorld_Start_m7D4140BFC9DCC632B4B2907C1BFFAC4FF2FE9247,
+	HelloWorld_Update_mD199B6A4EF418F7EBEE4AEC5645BB7B6215371C5,
+	HelloWorld__ctor_m7E681D25064FF84A25730DDBC04091394DE55C35,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	965,
+	965,
+	965,
+	965,
+	965,
+	965,
+	965,
+	965,
+	965,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
